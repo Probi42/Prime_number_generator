2558e2a5630a2cfa355422b5b4b72647d1cb5431
Probi42 <93741329+Probi42@users.noreply.github.com>
2021-11-04 22:19:23 -0300

    Add files via upload

---
+++ Prime numbers.py
@@ -0,0 +1,19 @@
+n = input('Show prime numbers up to: ')
+i = 3
+prime_numbers = [2]
+poss_primes = []
+while i < int(n):
+    for primes in prime_numbers:
+        poss_primes.append(i % primes)
+    if not any(poss_prime == 0 for poss_prime in poss_primes):
+        prime_numbers.append(i)
+    poss_primes.clear()
+    i += 1
+#print(prime_numbers)
+f = open('primes.txt', 'w+')
+for prime in prime_numbers:
+     f.write(str(prime) + '\n')
+f.close()
+import os
+file = 'primes.txt'
+os.system(file)
